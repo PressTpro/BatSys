9a4a11d7067bbc5cff31cd7843e24d2f671fb9d0
PressTpro <95140308+PressTpro@users.noreply.github.com>
2022-08-29 18:17:30 -0500

    Update manual.bat

--- manual.bat
+++ manual.bat
@@ -7,7 +7,15 @@ echo 1) Return to BatSys
 echo 2) Getting Started
 echo 3) Searching the Web with BBrowser
 set /p page=""
-goto return
+if %page% == 1 goto return
+if %page% == 2 goto getting-started
+if %page% == 3 goto bbrowser
+:: Pages
+:bbrowser
+title BBrowser - BatSys Manual
+echo BBrowser is a Built-In Search Browser you can use it to connect to webpages through microsoft edge
+pause
+goto index
 :return
 start batsys.bat
 exit
